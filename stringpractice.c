dfc0641d39c9893528e5d6eb871013b953e8cf23
KSK7368 <149864821+KSK7368@users.noreply.github.com>
2024-09-14 13:38:44 +0530

    Create stringpractice.c
    string practice

---
+++ stringpractice.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<string.h>
+
+int main()
+{ char i,name[20] = "SOFTWARE";
+ for(i=0;i<8;i++)
+  {
+      printf("%c",name[i]);
+  }
+ printf("\n");
+  for(i=4;i<5;i++)
+  {
+      printf("%c",name[i]);
+  }
+
+    return 0;
+}
